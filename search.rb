c370384c85191b6d0187f78e44bc46e740275169
Sarah Weaver <lwflouisa@gmail.com>
2021-01-23 04:37:59 -0500

    Update and rename alearn.rb to search.rb

--- search.rb
+++ search.rb
@@ -5,12 +5,4 @@ def search
   ALearn::LearnAnswer.bf_search
 end
 
-# Retrieves an answer instantly from training data.
-def answer
-  require "aLearn"
-
-  ALearn::LearnAnswer.find_answer
-end
-
 search
-# answer
